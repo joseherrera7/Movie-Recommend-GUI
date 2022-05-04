9f5abfc9217b0eb2beaa6228d67e30de4927fe93
Emmanuel Alvarado <josue0ghost@gmail.com>
2022-05-04 09:49:46 -0600

    [Add] recomendations flow

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -14,9 +14,10 @@ import { Router } from "@angular/router";
 export class MoviesComponent implements OnInit {
   public copy: string;
   public movies: Item[];
+  public recomendations: Item[];
+  public Title = 'Top todo el mundo'
 
-  private baseUrl =
-    "https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=";
+  private baseUrl = environment.protocolo + environment.server + ":" +environment.puerto;
   private httpOptions = {
     headers: new HttpHeaders({
       "Content-Type": "application/json",
@@ -37,73 +38,101 @@ export class MoviesComponent implements OnInit {
     if (!localStorage.getItem("user_email")) {
       this.router.navigate(['login']);
     }
-    
-    const { data } = await axios.get(
-      environment.protocolo +
-        environment.server +
-        ":" +
-        environment.puerto +
-        "/coldstart",
-      { headers: { "Access-Control-Allow-Origin": "*" } }
-    );
-    this.movies = data.movies;
-    console.log(this.movies);
-    /*this.movies = [
-      {
-        movie_title: "Batman v Superman: Dawn of Justice",
-        duration: "183",
-        director_name: "Zack Snyder",
-        title_year: "2016",
-        imdb_score: "6.9",
-        genres: ["Action", "Adventure", "Sci-Fi"],
-        img: "",
-      },
-      {
-        movie_title: "Batman v Superman: Dawn of Justice",
-        duration: "183",
-        director_name: "Zack Snyder",
-        title_year: "2016",
-        imdb_score: "6.9",
-        genres: ["Action", "Adventure", "Sci-Fi"],
-        img: "",
-      },
-      {
-        movie_title: "Batman v Superman: Dawn of Justice",
-        duration: "183",
-        director_name: "Zack Snyder",
-        title_year: "2016",
-        imdb_score: "6.9",
-        genres: ["Action", "Adventure", "Sci-Fi"],
-        img: "",
-      },
-    ];*/
 
-    /*for (let index = 0; index < this.movies.length; index++) {
-      let json = this.getPoster(this.movies[index].movie_title);
-      this.movies[index].img =
-        "http://image.tmdb.org/t/p/w500/" + json[0].poster_path;
-    }*/
+    // Recomendar al inicio de sesión
+    if (localStorage.getItem("recomendations") != "null") {   
+      let recomendations = localStorage.getItem("recomendations")
+      let generos = recomendations.split('|')
+
+      const { data } = await axios.post(
+        this.baseUrl + "/knnGenreBased", 
+        { genres: generos },
+        { headers: { "Access-Control-Allow-Origin": "*" } }
+      );
+
+      this.Title = 'Top para ti'
+      this.movies = data.recommendations;
+    }
+    else {
+
+      const { data } = await axios.get( 
+        this.baseUrl + "/coldstart", 
+        { headers: { "Access-Control-Allow-Origin": "*" } }
+      );
+
+      this.movies = data.movies.map(function(x) {
+        return {
+          imdb_score: Number.parseFloat(x.imdb_score).toFixed(2),
+          movie_title: x.movie_title.trim(),
+          num_critic_for_reviews: x.num_critic_for_reviews,
+          score: Number.parseFloat(x.score).toFixed(2)
+        }
+      });
+    }
   }
 
   async encontrarRecomendaciones(movie_title) {
-    console.warn(movie_title);
     const { data } = await axios.post(
-      environment.protocolo +
-        environment.server +
-        ":" +
-        environment.puerto +
-        "/recommendations",
-      { movie_title },
+      this.baseUrl + "/recommendations", 
+      { movie_title: movie_title },
       this.options
     );
-    this.movies = data.movies;
-    console.log(this.movies);
+
+    this.recomendations = data.recommendations;
   }
 
-  getPoster(movie_title: string): Observable<object> {
-    return this.http.get(
-      this.baseUrl + movie_title + "&callback=?",
-      this.httpOptions
+  async agregarAFavoritos(genres, movie_title) {
+    // si la película tiene el campo de géneros
+    if (genres) {
+      let generos = genres.split('|')
+
+      const { data } = await axios.post(
+        this.baseUrl +"/knnGenreBased", 
+        { genres: generos },
+        this.options
+      );
+      
+      this.recomendations = data.recommendations;
+
+      // Agregar recomendaciones al usuario en BD
+      const result = await axios.post(
+        "https://9v7tw9x26a.execute-api.us-east-1.amazonaws.com/dev/recomendations", 
+        { 
+          action: "add",
+          email: localStorage.getItem("user_email"),
+          genres: genres
+        }
+      );
+
+      if (result.data != "Error") {
+        console.log(result.data)
+        localStorage.setItem("recomendations", result.data)
+      }
+    }
+    else {
+      const { data } = await axios.post(
+        this.baseUrl + "/recommendations", 
+        { movie_title: movie_title },
+        this.options
+      );
+
+      this.recomendations = data.recommendations;
+    }
+  }
+
+  async quitarDeFavoritos(genres) {
+    // Agregar recomendaciones al usuario en BD
+    const result = await axios.post(
+      "https://9v7tw9x26a.execute-api.us-east-1.amazonaws.com/dev/recomendations", 
+      { 
+        action: "remove",
+        email: localStorage.getItem("user_email"),
+        genres: genres
+      }
     );
+
+    if (result.data != "Error") {
+      localStorage.setItem("recomendations", result.data)
+    }
   }
 }
